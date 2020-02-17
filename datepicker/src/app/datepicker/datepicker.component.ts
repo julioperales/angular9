d96d033d487c244ca257ae5d21114336dd3e675f
juldelos <julio.delosperales@prodigious.com>
2020-02-17 15:16:15 +0100

    base calendar

--- datepicker/src/app/datepicker/datepicker.component.ts
+++ datepicker/src/app/datepicker/datepicker.component.ts
@@ -13,6 +13,8 @@ export class DatepickerComponent implements OnInit {
   localeString: string = 'es';
   navDate: any;
   weekDaysHeaderArr: Array<string> = [];
+  gridArr: Array<any> = [];
+  selectedDate: any;
 
     constructor() {
         
@@ -21,14 +23,14 @@ export class DatepickerComponent implements OnInit {
   ngOnInit(): void {
     moment.locale(this.localeString);              
     this.navDate = moment();
-
-    this.makeHeader();
-    
+    this.weekDaysHeaderArr = moment.weekdays(true);    
+    this.makeGrid();
   }
 
   changeNavMonth(num: number){
     if(this.canChangeNavMonth(num)){
       this.navDate.add(num, 'month');
+      this.makeGrid();
     }
   }
 
@@ -41,9 +43,50 @@ export class DatepickerComponent implements OnInit {
     return clonedDate.isBetween(minDate, maxDate);
   }
 
-  makeHeader(){
-    const weekDaysArr: Array<number> = [0, 1, 2, 3, 4, 5, 6];
-    weekDaysArr.forEach(day => this.weekDaysHeaderArr.push(moment().weekday(day).format('ddd')));
+  makeGrid(){
+    this.gridArr = [];
+
+    const firstDayDate = moment(this.navDate).startOf('month');
+    const initialEmptyCells = firstDayDate.weekday();
+    const lastDayDate = moment(this.navDate).endOf('month');
+    const lastEmptyCells = 6 - lastDayDate.weekday();
+    const daysInMonth = this.navDate.daysInMonth();
+    const arrayLength = initialEmptyCells + lastEmptyCells + daysInMonth;
+
+    for(let i = 0; i < arrayLength; i++){
+      let obj: any = {};
+      if(i < initialEmptyCells || i > initialEmptyCells + daysInMonth -1){
+        obj.value = 0;
+        obj.available = false;
+      } else {
+        obj.value = i - initialEmptyCells +1;
+        obj.available = this.isAvailable(i - initialEmptyCells +1);
+      }
+      this.gridArr.push(obj);
+    }
   }
+
+  isAvailable(num: number): boolean{
+    let dateToCheck = this.dateFromNum(num, this.navDate);
+    if(dateToCheck.isBefore(moment(), 'day')){
+        return false;
+    } else {
+        return true;
+    }
+  }
+
+  dateFromNum(num: number, referenceDate: any): any{
+    let returnDate = moment(referenceDate);
+    return returnDate.date(num);
+  }
+
+  selectDay(day: any){
+    if(day.available){
+      this.selectedDate = this.dateFromNum(day.value, this.navDate);
+    }
+  }
+
+
+
 
 }
